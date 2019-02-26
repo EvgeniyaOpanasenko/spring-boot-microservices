ab19edb2c59e5f426383b57a4f58693ff3c94627
Eva <evgeniya.opanasenko@outlook.com>
2019-02-26 13:48:05 +0200

    Using Ribbon for Load Balancing

--- spring-boot-microservice-currency-conversion/src/main/java/com/ua/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ spring-boot-microservice-currency-conversion/src/main/java/com/ua/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -2,8 +2,12 @@ package com.ua.springboot.microservice.example.currencyconversion.springbootmicr
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.ua.springboot.microservice.example.currencyconversion")
+@EnableDiscoveryClient
 public class SpringBootMicroserviceCurrencyConversionApplication {
 
 	public static void main(String[] args) {
